5a27febc7d86683b92e217f86e56ef68e652da06
oludare89 <azizoyekan@hotmail.com>
2021-03-28 13:23:34 +0000

    Installed Flask-PyMongo and dnspython to establish link to Mongo database

--- app.py
+++ app.py
@@ -4,11 +4,19 @@ Initialisation of flask app and server routes
 import os
 from flask import (Flask, flash, render_template, redirect,
                     request, session, url_for, abort)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 
 @app.route("/")
