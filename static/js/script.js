0dfe983310dd020eccc25fa5db238116a0919003
oludare89 <azizoyekan@hotmail.com>
2021-04-24 23:33:43 +0000

    Added dropdown in navbar for when users are logged to access their profile or to logout

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
+    $(".dropdown-trigger").dropdown();
+    $(".collapsible").collapsible();
 
     $('#flash-close').click(function() {
         $(".flashes").addClass("hide");
