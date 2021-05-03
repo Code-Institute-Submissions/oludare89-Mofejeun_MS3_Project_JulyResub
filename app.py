0b98e9445c45c0f82f4a1b6425fa17ee0076ac99
oludare89 <azizoyekan@hotmail.com>
2021-05-03 23:17:36 +0000

    Updated jinja on recipe.html and added recipe function in app.py

--- app.py
+++ app.py
@@ -161,6 +161,13 @@ def edit_recipe(recipe_id):
     return render_template("edit_recipe.html", recipe=recipe)
 
 
+@app.route("/recipe/<recipe_id>", methods = ["GET", "POST"])
+def recipe(recipe_id):
+    
+    recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    return render_template("edit_recipe.html", recipe=recipe)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
